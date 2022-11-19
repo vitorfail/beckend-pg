abff0b300ee2d355dc294e9fe81f144d17128723
vitorfail <vitor_andrademanoel@hotmail.com>
2022-11-19 19:00:37 -0300

    direcionando squlize par o link db

--- db.tsx
+++ db.tsx
@@ -1,5 +1,6 @@
 const config = require('./config.tsx')
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(config);
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = sequelize
